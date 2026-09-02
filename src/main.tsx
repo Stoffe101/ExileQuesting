c67c1f3a177ca2cc64302ff318036de3b7ab525b
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-02 15:59:53 +0200

    Load Maxroll coach styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './ui/App';
 import './ui/styles.css';
+import './ui/maxroll.css';
 import './ui/reliability.css';
 import './ui/responsive.css';
 import './ui/lab.css';
